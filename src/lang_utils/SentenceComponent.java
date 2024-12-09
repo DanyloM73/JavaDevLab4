0fa77fe56215d4c7c18703ad7d1e1d25e1ef9b08
Danylo Malanin <115399817+DanyloM73@users.noreply.github.com>
2024-12-09 12:42:58 +0200

    Add documentation for SentenceComponent interface

--- src/lang_utils/SentenceComponent.java
+++ src/lang_utils/SentenceComponent.java
@@ -1,6 +1,13 @@
 package lang_utils;
 
+/**
+ * Represents a component of a sentence, such as a word or a punctuation mark.
+ * Each implementing class must provide a string representation of the component.
+ */
 public interface SentenceComponent {
+    /**
+     * @return the string representation of the component
+     */
     @Override
     String toString();
 }
